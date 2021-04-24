e7b7ee18f5d19b5ea83b4339d307cfffab31ce95
fahrianur19630387 <fahrianur32@gmail.com>
2021-04-24 16:30:58 +0800

    Operator Binary Aritmatika

--- src/Operator/OperatorBinary.java
+++ src/Operator/OperatorBinary.java
@@ -5,4 +5,13 @@ public class OperatorBinary {
     String NPM = "19630387";
     int jumlahSKS = 24;
     double IndeksPresatasiKumulatif = 3.77;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9/3);
+        System.out.println(9*3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
