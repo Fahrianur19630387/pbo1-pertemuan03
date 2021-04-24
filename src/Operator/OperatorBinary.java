ac793248580b309d3558a1ffab350edc6dbaccbb
fahrianur19630387 <fahrianur32@gmail.com>
2021-04-24 16:28:46 +0800

    Operator Binary Assignment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,8 @@
+package Operator;
+
+public class OperatorBinary {
+    String nama = "Fahrianur";
+    String NPM = "19630387";
+    int jumlahSKS = 24;
+    double IndeksPresatasiKumulatif = 3.77;
+}
